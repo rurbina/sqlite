e1f64a5b974149b1ce3842622539e0d5d2084e77
Ricardo Rat Urbina <rat@sdf.org>
2015-07-22 11:57:13 -0500

    Estaba mal mi cuenta de parámetros

--- dice.c
+++ dice.c
@@ -40,16 +40,16 @@ int sqlite3_dice_init(sqlite3 *db, char **pzErrMsg, const sqlite3_api_routines *
   sqlite3_create_function_v2(db, "roll", 1, SQLITE_UTF8, NULL, sqlite_ext_roll, NULL, NULL, NULL );
   sqlite3_create_function_v2(db, "roll", 2, SQLITE_UTF8, NULL, sqlite_ext_roll, NULL, NULL, NULL );
   sqlite3_create_function_v2(db, "roll", 3, SQLITE_UTF8, NULL, sqlite_ext_roll, NULL, NULL, NULL );
-  sqlite3_create_function_v2(db, "dice_avg", 1, SQLITE_UTF8 | SQLITE_DETERMINISTIC, NULL, sqlite_ext_dice_avg, NULL, NULL, NULL );
-  sqlite3_create_function_v2(db, "dice_avg", 2, SQLITE_UTF8 | SQLITE_DETERMINISTIC, NULL, sqlite_ext_dice_avg, NULL, NULL, NULL );
-  sqlite3_create_function_v2(db, "dice_avg", 3, SQLITE_UTF8 | SQLITE_DETERMINISTIC, NULL, sqlite_ext_dice_avg, NULL, NULL, NULL );
+  sqlite3_create_function_v2(db, "dice_avg", 1, SQLITE_UTF8, NULL, sqlite_ext_dice_avg, NULL, NULL, NULL );
+  sqlite3_create_function_v2(db, "dice_avg", 2, SQLITE_UTF8, NULL, sqlite_ext_dice_avg, NULL, NULL, NULL );
+  sqlite3_create_function_v2(db, "dice_avg", 3, SQLITE_UTF8, NULL, sqlite_ext_dice_avg, NULL, NULL, NULL );
   sqlite3_create_function_v2(db, "dice_seed", 1, SQLITE_UTF8, NULL, sqlite_ext_seed, NULL, NULL, NULL );
   return rc;
 }
 
-void sqlite_ext_seed(sqlite3_context *db, int row, sqlite3_value **value) {
+void sqlite_ext_seed(sqlite3_context *db, int argc, sqlite3_value **argv) {
 
-  int  seed   = sqlite3_value_int(value[0]);
+  int  seed   = sqlite3_value_int(argv[0]);
   srandom(seed);
 
   return;
@@ -57,7 +57,9 @@ void sqlite_ext_seed(sqlite3_context *db, int row, sqlite3_value **value) {
 
 int parse_dice(char *dice, int *qty, int *sides, int *plus) {
 
-  int parsed = sscanf(dice, "%id%i%i", qty, sides, plus);
+  int parsed = sscanf(dice, "%i d %i%i", qty, sides, plus);
+
+  printf("tried to parse %s, got %d\n", dice, parsed);
 
   if ( parsed < 2 ) return 0;
 
@@ -66,7 +68,7 @@ int parse_dice(char *dice, int *qty, int *sides, int *plus) {
   }
 
   return ( *qty > 0 && sides > 0 );
-  
+
 }
 
 int roll_dice(int qty, int sides, int plus) {
@@ -78,27 +80,27 @@ int roll_dice(int qty, int sides, int plus) {
   }
 
   return total;
-  
+
 }
 
-void sqlite_ext_roll(sqlite3_context *db, int row, sqlite3_value **value) {
+void sqlite_ext_roll(sqlite3_context *db, int argc, sqlite3_value **argv) {
 
-  int qty, sides, plus, parse;
+  int qty, sides, plus, parse = 1;
 
-  if ( value[1] == NULL ) {
-    parse = parse_dice((char *)sqlite3_value_text(value[0]), &qty, &sides, &plus);
+  if ( argc == 1 ) {
+    parse = parse_dice((char *)sqlite3_value_text(argv[0]), &qty, &sides, &plus);
   }
   else {
-    qty   = sqlite3_value_int(value[0]);			 
-    sides = sqlite3_value_int(value[1]);			 
-    plus  = value[2] == NULL ? 0 : sqlite3_value_int(value[2]);
+    qty   = sqlite3_value_int(argv[0]);
+    sides = sqlite3_value_int(argv[1]);
+    plus  = ( argc == 2 ) ? 0 : sqlite3_value_int(argv[2]);
   }
 
-  if ( qty < 1 || sides < 1 )
-    sqlite3_result_int(db, 0);
+  if ( !parse || qty < 1 || sides < 1 )
+    sqlite3_result_error(db, "Bad dice spec", -1);
   else
     sqlite3_result_int(db, roll_dice(qty, sides, plus) );
-  
+
 }
 
 double dice_avg(int qty, int sides, int plus) {
@@ -106,26 +108,28 @@ double dice_avg(int qty, int sides, int plus) {
   double total = plus + ( ( (sides / 2) + .5 ) * qty );
 
   return total;
-  
+
 }
 
-void sqlite_ext_dice_avg(sqlite3_context *db, int row, sqlite3_value **value) {
+void sqlite_ext_dice_avg(sqlite3_context *db, int argc, sqlite3_value **argv) {
 
-  int qty, sides, plus, parse;
+  int qty, sides, plus, parse = 1;
+  char err[1024];
 
-  if ( value[1] == NULL ) {
-    parse = parse_dice((char *)sqlite3_value_text(value[0]), &qty, &sides, &plus);
+  if ( argc == 1 ) {
+    parse = parse_dice((char *)sqlite3_value_text(argv[0]), &qty, &sides, &plus);
   }
   else {
-    qty   = sqlite3_value_int(value[0]);			 
-    sides = sqlite3_value_int(value[1]);			 
-    plus  = value[2] == NULL ? 0 : sqlite3_value_int(value[2]);
+    qty   = sqlite3_value_int(argv[0]);
+    sides = sqlite3_value_int(argv[1]);
+    plus  = ( argc == 2 ) ? 0 : sqlite3_value_int(argv[2]);
   }
 
-  if ( qty < 1 || sides < 1 )
-    sqlite3_result_int(db, 0);
-  else    
+  if ( !parse || qty < 1 || sides < 1 ) {
+    sprintf(err, "bad dice spec: `%s'", (char *)sqlite3_value_text(argv[0]));
+    sqlite3_result_error(db, err, -1);
+  }
+  else
     sqlite3_result_double(db, dice_avg(qty, sides, plus) );
-  
-}
 
+}
